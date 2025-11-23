0d33c2a90be8684b06153e4d7151d5c389c5cc33
Ignacio <alumno.770209@ies-azarquiel.es>
2025-11-23 13:43:56 +0100

    fourthcommit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  root: "./src", // Cambia la raíz del proyecto a "src"
+  root: "src",
+  base: "/ZetaBot/", // <--- importante
+  publicDir: resolve(__dirname, "public"),
   build: {
-    outDir: "../docs", // Los archivos construidos irán a la carpeta "docs"
+    outDir: resolve(__dirname, "docs"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "src/index.html"),
+        formulario: resolve(__dirname, "src/formulario.html"),
+        chat: resolve(__dirname, "src/chat.html"),
+      },
+    },
   },
 });
